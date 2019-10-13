9de8a72aaf66a79141e4f3738618672342846134
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-10-13 11:07:33 -0300

    "Criada logica logica para gerar frases aleatorias."
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: Matheu de Cristo Doreia Estanislau <matheus.estanislau7@gmail.com>

--- horoscopo.pl
+++ horoscopo.pl
@@ -10,17 +10,14 @@ menu :- write('Bem vindo ao Horóscopo!'), nl,
 opcao(1) :- userData().
 
 % if Op == 2:
-opcao(2) :- ascendente().
-
-% if Op == 3:
-opcao(4) :- write('Até mais!'),nl,
+opcao(2) :- write('Até mais!'),nl,
             halt.
 
 userData() :-
-            write('Digite o mes de seu nascimento: '),nl,
-            read(Mes),
             write('Digite o dia de seu nascimento: '),nl,
             read(Dia),
+            write('Digite o mes de seu nascimento: '),nl,
+            read(Mes),
             signo(Mes,Dia).
 
 signo(1,Dia):- 
@@ -59,18 +56,108 @@ signo(11,_):- sargitario().
 
 signo(12,Dia):-  Dia =< 21, sargitario().
 
-signo(12,_):- sargitario().
+signo(12,Dia):- Dia > 21, capricornio().
 
 
-capricornio():-write("Capricornio\n").
-aquario():- write("Aquario\n").
-peixe():- write("Peixe\n").
-sargitario():- write("Sargitário\n").
-escorpiao():- write("Escorpião\n").
-virgem():- write("Virgem\n").
-touro():- write("Touro\n").
-leao():- write("Leão\n").
-libra():- write("Libra\n").
-cancer():- write("Cancer\n").
-aries():- write("Aries\n").
-gemeos():- write("Aries\n").
+capricornio():-
+        write("Seu signo é Capricornio.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+        
+aquario():- 
+        write("Seu signo é Aquario.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+        
+peixe():- 
+        write("Seu signo é Peixe.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+sargitario():-
+        write("Seu signo é Sargitário.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+escorpiao():-
+         write("Seu signo é Escorpião.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+virgem():- 
+        write("Seu signo é Virgem.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+touro():- 
+        write("Seu signo é Touro.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+leao():-
+        write("Seu signo é Leão.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+        
+libra():- 
+        write("Seu signo é Libra.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+cancer():- 
+        write("Seu signo é Cancer.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+aries():- 
+        write("Seu signo é Aries.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+gemeos():- 
+        write("Seu signo é Gemeos.\n"), 
+        random(0,2,X), 
+        gerarFrase(X).
+
+
+gerarFrase(0):-
+            write("Você terá "),
+            random(0,4,X),
+            seleciona(['amor.','novas amizades.', 'companhia.', 'azar no amor.','inimizades.'],X,H).
+
+gerarFrase(1):-
+            write('Você deve Buscar '),
+            random(0,2,X),
+            seleciona(['se afastar de ','se aproximar de ','investir mais em vc e '],X,H),
+            random(0,2,Y),
+            seleciona(['amizades antigas.','amizades novas.','relacionamento.'],Y,H).
+
+gerarFrase(2):-
+            write("Você conseguirá "),
+            random(0,2,X),
+            seleciona(['prosperidade.', 'paz.', 'emprego desejado.'],X,H).
+
+imprimeQuestao().
+imprimeQuestao(X):-
+        format("~w\n", X).
+
+
+seleciona([X|_],0,H):-
+        imprimeQuestao(X),!.
+         
+seleciona([_|Z],Y,H):-
+        Y > 0, 
+        Y1 is Y-1,
+        seleciona(Z,Y1,H). 
+
+% Você terá ','Você deve Buscar ', 'Você conseguirá']
+
+% voce tera
+%['amor ','amizade ', 'companhia', 'azar','inimizades']
+
+% vc deve buscar 
+%['se afastar de ','se aproximar de ','investir mais em vc e ']
+%['amizades antigas.','amizades novas.','relacionamento.']
+
+% voce conseguirá
+%['prosperidade', 'paz', 'emprego desejado', '']
